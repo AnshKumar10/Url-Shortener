b50745e54c36e2ef3080ae0441d84ee4e4572846
ansh <ansh@onbo.today>
2024-10-04 09:15:27 +0530

    Initial repo setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-red-500 text-2xl">Initial Setup - Url Shortener</div>
+    </>
+  );
+}
+
+export default App;
